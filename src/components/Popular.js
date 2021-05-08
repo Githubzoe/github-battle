3449427695623c16462410971a9947821d674614
Ye Shi <zoe.shiye@gmail.com>
2021-05-09 03:11:20 +1000

    added css to repos

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -72,7 +72,7 @@ function ReposGrid({repos}){
               </li>
               <li>
                 <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                {open_issues.toLocaleString()} open open_issues
+                {open_issues.toLocaleString()} open issues
               </li>
             </ul>
           </li>
