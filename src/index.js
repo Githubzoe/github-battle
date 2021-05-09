d3d3e1ff18a607bc9af49134f55a2c5e13e3a4c0
Ye Shi <zoe.shiye@gmail.com>
2021-05-09 21:36:39 +1000

    added battle components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Battle from './components/Battle'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Battle />
   </React.StrictMode>,
   document.getElementById('root')
 );
