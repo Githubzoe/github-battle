e8c45c1b5d172f67559067349b42be70d9a28654
Ye Shi <zoe.shiye@gmail.com>
2021-05-10 00:58:37 +1000

    rendered player input component

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
 import PropTypes from 'prop-types'
 
@@ -86,13 +86,46 @@ PlayerInput.propTypes={
   label: PropTypes.string.isRequired
 }
 
-export default class Battle extends Component {
+export default class Battle extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      playerOne: null,
+      playerTwo: null,
+    }
+    this.handleSubmit=this.handleSubmit.bind(this)
+  }
+  handleSubmit(id, player){
+    this.setState({
+      [id]: player
+    })
+  }
   render() {
+    const { playerOne, playerTwo } = this.state;
+    
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput label="Player" onSubmit={(value)=>console.log('Player', value)} />
+
+        <div className="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne===null && (
+              <PlayerInput 
+                label="Player One" 
+                onSubmit={(player)=>this.handleSubmit('playerOne', player)} 
+              />
+            )}
+            {playerTwo===null && (
+              <PlayerInput 
+                label="Player Two" 
+                onSubmit={(player)=>this.handleSubmit('playerTwo', player)}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
-    );
+    )
   }
 }
