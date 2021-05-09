fa1802a9c57f256351fb1500a14c9dc7078726d1
Ye Shi <zoe.shiye@gmail.com>
2021-05-09 23:19:53 +1000

    added player input component

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
-import {FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa'
+import React, { Component } from "react";
+import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes from 'prop-types'
 
-function Instructions(){
+function Instructions() {
   return (
     <div className="instructions-container">
       <h1 className="center-text header-lg">Instructions</h1>
@@ -33,12 +34,65 @@ function Instructions(){
   );
 }
 
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.username)
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <form className="column player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            className="input-light"
+            placeholder="github username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <button className="btn dark-btn" type="submit" disabled={!this.state.username}>
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes={
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+}
+
 export default class Battle extends Component {
   render() {
     return (
       <React.Fragment>
         <Instructions />
+        <PlayerInput label="Player" onSubmit={(value)=>console.log('Player', value)} />
       </React.Fragment>
-    )
+    );
   }
 }
